0fdebbf98742900dcf5020bd1eb39e71275b48f3
Danni <d.youziel@gmail.com>
2019-04-16 18:40:11 +0100

    Add delete routes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
 
-//mongose requests always return 200 even if request
-//was successful even if nothing is returned
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -52,6 +49,51 @@ app.get('/users/:id', async (req,res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password'];
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!_id.match(/^[0-9a-fA-F]{24}$/) || !isValidOperation) {
+        return res.status(400).send({Error: 'Invalid Update!'});
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true} );
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    if (!_id.match(/^[0-9a-fA-F]{24}$/)) {
+        return res.status(400).send({Error: 'Invalid Update!'});
+    }
+
+    try {
+        const user = await User.findByIdAndDelete(_id); 
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send();
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
@@ -91,6 +133,51 @@ app.get('/tasks/:id', async (req, res) => {
         res.status(500).send();
     }
 
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!_id.match(/^[0-9a-fA-F]{24}$/) || !isValidOperation) {
+        return res.status(400).send({Error: 'Invalid Update!'});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true} );
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    if (!_id.match(/^[0-9a-fA-F]{24}$/)) {
+        return res.status(400).send({Error: 'Invalid Update!'});
+    }
+
+    try {
+        const task = await Task.findByIdAndDelete(_id); 
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (error) {
+        res.status(500).send();
+    }
 })
 
 app.listen(port, () => {
